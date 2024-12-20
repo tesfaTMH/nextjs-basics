61a2ee08c45882ea26537d661462d674ea3e5e53
tesfaTMH <hagos.tesfam@gmail.com>
2024-12-20 21:21:35 +0200

    create layout and possible routes for nextlevel food app

---
+++ app/community/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>community route</div>;
+};
+
+export default page;
---
+++ app/meals/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>meals route </div>;
+};
+
+export default page;
